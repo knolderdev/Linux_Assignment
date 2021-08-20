41e2408cd4ee524c35e84005ed5b5939ebf2a0e9
devanshu <devanshu.verma@knoldus.com>
2021-08-20 18:51:57 +0530

    Initial Commit

---
+++ assign.sh
@@ -0,0 +1,29 @@
+#Exercise 1
+echo "Shell Scripting is Fun!"
+
+#Exercise 2
+sentance=Shell Scripting is Fun!
+echo $sentance
+
+#Exercise 3
+h=$HOSTNAME
+echo "This script is running on $h"
+
+#Exercise 4
+A="human pig dog cat sheep deer bear"
+
+for i in $A
+do
+       echo "$i"
+done
+
+#Exercise 5
+echo "This script will exit with 0 status."
+exit 0
+
+#Exercise 6
+function count() {
+    local NOF=$(ls -l | wc -l)
+    echo The number of files are "$NOF"
+}
+count
